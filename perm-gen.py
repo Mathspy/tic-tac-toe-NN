b3857fc63a80b5077ac66faf0295621b25327ba7
Mathspy <mathspy257@gmail.com>
2018-07-21 18:29:33 +0200

    returning winner from isGameOver

--- perm-gen.py
+++ perm-gen.py
@@ -13,13 +13,14 @@ def isGameOver(board):
     for x in range (0, 3):
         y = x * 3
         if (board[y] != Cell.EMPTY and board[y] == board[(y + 1)] and board[y] == board[(y + 2)]) :
-            return True
+            return board[y]
         if (board[x] != Cell.EMPTY and board[x] == board[(x + 3)] and board[x] == board[(x + 6)]) :
-            return True
+            return board[x]
 
-    if((board[0] != Cell.EMPTY and board[0] == board[4] and board[0] == board[8]) or
-       (board[2] != Cell.EMPTY and board[2] == board[4] and board[4] == board[6])) :
-        return True
+    if (board[0] != Cell.EMPTY and board[0] == board[4] and board[0] == board[8]):
+        return board[0]
+    if (board[2] != Cell.EMPTY and board[2] == board[4] and board[4] == board[6]):
+        return board[2]
 
     return False
 

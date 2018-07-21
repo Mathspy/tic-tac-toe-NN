c0797a7c7e1ce2822f1ca707536d2d119856046f
Mathspy <mathspy257@gmail.com>
2018-07-21 20:00:14 +0200

    Finished Permutations Generator
    - Created a noMoreMovesLeft function
    - Used a translation of my older JavaScript code to findBestMoves
    - Updated validity detection to detect boards without moves left
    - Turned all_posibilities into a list of lists (insted of tubles) for easily using in findBestMove function
    - Wrote a numpy representation of inputs and labels into pickles

--- perm-gen.py
+++ perm-gen.py
@@ -1,3 +1,7 @@
+import math
+import numpy as np
+import pickle
+
 from itertools import product
 from enum import Enum
 
@@ -6,7 +10,7 @@ class Cell(Enum):
     FIRST_PLAYER = 'X'
     SECOND_PLAYER = 'O'
 
-all_possibilities = list(product([Cell.EMPTY, Cell.FIRST_PLAYER, Cell.SECOND_PLAYER], repeat=9))
+all_possibilities = [list(board) for board in product([Cell.EMPTY, Cell.FIRST_PLAYER, Cell.SECOND_PLAYER], repeat=9)]
 print(len(all_possibilities))
 
 def isGameOver(board):
@@ -33,7 +37,7 @@ def isValid(board):
     # is same or xount is one more than oCount
     if (firstPlayerCount == secondPlayerCount or firstPlayerCount == secondPlayerCount + 1):
         # Check if gameOver then permutation is not needed
-        if (isGameOver(board)):
+        if (isGameOver(board) or noMoreMovesLeft(board)):
             return False
 
         # If counts are correct and game isn't over
@@ -41,8 +45,53 @@ def isValid(board):
 
     return False
 
+def noMoreMovesLeft(board):
+    return True if board.count(Cell.EMPTY) == 0 else False
+
+
+def findBestMove(board):
+    me = Cell.FIRST_PLAYER if board.count(Cell.FIRST_PLAYER) == board.count(Cell.SECOND_PLAYER) else Cell.SECOND_PLAYER
+    bestMove = (None, -math.inf)
+    for idx, cell in enumerate(board):
+        if (cell == Cell.EMPTY):
+            newBoard = board.copy()
+            newBoard[idx] = me
+            moveValue = minimax(newBoard, 0, me, me)
+            if (bestMove[1] < moveValue):
+                bestMove = (idx, moveValue)
+
+    return bestMove[0]
+
+def minimax(board, depth, me, turn):
+    winner = isGameOver(board)
+    if (winner):
+        if (winner == me):
+            return 10 - depth
+        else:
+            return -10 + depth
+    elif (noMoreMovesLeft(board)):
+        return 0
+
+    turn = Cell.FIRST_PLAYER if turn == Cell.SECOND_PLAYER else Cell.SECOND_PLAYER
+
+    bestValue = -math.inf
+    for idx, cell in enumerate(board):
+        if (cell == Cell.EMPTY):
+            newBoard = board.copy()
+            newBoard[idx] = turn
+            value = minimax(newBoard, depth+1, me, turn)
+            bestValue = max(bestValue, value) if me == turn else min(bestValue, value)
+
+    return bestValue
+
+
 def byteifyPossibility(board):
     return [(1 if cell == Cell.FIRST_PLAYER else 0) for cell in board] + [(1 if cell == Cell.SECOND_PLAYER else 0) for cell in board]
 
-valid_possibilities = [byteifyPossibility(board) for board in all_possibilities if isValid(board)]
-print(len(valid_possibilities))
+valid_possibilities = [board for board in all_possibilities if isValid(board)]
+train_x = np.array([byteifyPossibility(board) for board in valid_possibilities])
+train_y = np.array([findBestMove(board) for board in valid_possibilities])
+print(train_x.shape)
+print(train_y.shape)
+pickle.dump(train_x, open("./data/train_inputs.pickle", "wb"))
+pickle.dump(train_y, open("./data/train_labels.pickle", "wb"))
